21aa3e603903367ff0019462130b070a7717f38d
julianxlozano <julianxlozano@gmail.com>
2021-04-02 15:51:37 -0500

    commit tree d96bdc764461e06545f1bb88c867c2d0f32f88fc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do 
+        erb :super_hero
+    end
+
+    post '/teams' do 
+       # binding.pry
+        @team = params["team"]
+       # binding.pry
+        @team_members = params["team"]["member"]
+        erb :team
+    end
+
 
 
 end
